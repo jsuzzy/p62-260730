26ae67e7b496755c3b9b1c37e3a54c11271ddd0f
jeongsuji <sujijeong11@gmail.com>
2026-07-30 12:08:33 +0900

    ddl-auto update

--- src/main/java/com/back/p_62_260730/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/p_62_260730/domain/wiseSaying/entity/WiseSaying.java
@@ -1,6 +1,8 @@
 package com.back.p_62_260730.domain.wiseSaying.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,10 +10,11 @@ import lombok.Setter;
 
 @Setter
 @Getter
-@Entity //JPA가 db에 클래스에 해당하는 테이블 생성
-@NoArgsConstructor //JPA에는 무조건 필요?
+@Entity
+@NoArgsConstructor
 public class WiseSaying {
-    @Id //식별하고자 하는 데이터 위에 붙임
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id; // int
     private String content; // varchar(255)
     private String author; // varchar(255)
