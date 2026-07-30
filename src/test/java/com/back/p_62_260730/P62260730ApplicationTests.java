891784da23507c2f6ecc84fe1af712b73b4e68fe
jeongsuji <sujijeong11@gmail.com>
2026-07-30 11:11:04 +0900

    init

---
+++ src/test/java/com/back/p_62_260730/P62260730ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.p_62_260730;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P62260730ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
