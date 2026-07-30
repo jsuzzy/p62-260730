c7c21e0c9856456f1b0d9d387d9013df1a6a1ed9
jeongsuji <sujijeong11@gmail.com>
2026-07-30 14:47:46 +0900

    PostRepository

---
+++ src/main/java/com/back/p_62_260730/domain/wiseSaying/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.back.p_62_260730.domain.wiseSaying.repository;
+
+import com.back.p_62_260730.domain.wiseSaying.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
