efd35c21bca17c3b252770731749ed69adc14dbd
jeongsuji <sujijeong11@gmail.com>
2026-07-31 11:24:55 +0900

    baseInit requiredArgsConstructor

--- src/main/java/com/back/p_62_260730/global/BaseInit.java
+++ src/main/java/com/back/p_62_260730/global/BaseInit.java
@@ -1,16 +1,16 @@
 package com.back.p_62_260730.global;
 
 import com.back.p_62_260730.domain.post.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //빈 등록을 위한 클래스
+@RequiredArgsConstructor
 public class BaseInit {
 
-    @Autowired
-    private PostService postService;
+    public final PostService postService;
 
     //람다 방식
     @Bean
