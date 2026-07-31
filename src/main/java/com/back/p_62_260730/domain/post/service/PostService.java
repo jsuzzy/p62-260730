619a2d8ba5fd2d2e1f7dfa2582e23e86dcf740f9
jeongsuji <sujijeong11@gmail.com>
2026-07-31 12:17:19 +0900

    delete transaction test

--- src/main/java/com/back/p_62_260730/domain/post/service/PostService.java
+++ src/main/java/com/back/p_62_260730/domain/post/service/PostService.java
@@ -28,4 +28,8 @@ public class PostService {
     public long count(){
         return postRepository.count();
     }
+
+    public void delete(Post post) {
+        postRepository.delete(post);
+    }
 }
--- src/main/java/com/back/p_62_260730/global/BaseInit.java
+++ src/main/java/com/back/p_62_260730/global/BaseInit.java
@@ -1,15 +1,22 @@
 package com.back.p_62_260730.global;
 
+import com.back.p_62_260730.domain.post.entity.Post;
 import com.back.p_62_260730.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 
 @Configuration //빈 등록을 위한 클래스
 @RequiredArgsConstructor
 public class BaseInit {
+
+    @Autowired
+    @Lazy
+    private BaseInit self;
 
     public final PostService postService;
 
@@ -20,6 +27,10 @@ public class BaseInit {
             System.out.println("초기화 작업을 수행합니다.");
             work1();
             work2();
+
+            new Thread(() -> {
+                self.work3();
+            }).start();
         };
     }
 
@@ -33,6 +44,7 @@ public class BaseInit {
 
 
         postService.write("제목1", "내용1");
+
         postService.write("제목2", "내용2");
 
     }
@@ -41,5 +53,17 @@ public class BaseInit {
     void work2() {
         postService.findById(1);
         // select * from post where id = 1;
+    }
+
+    @Transactional
+    void work3() {
+        Post post1 = postService.findById(1).get();
+        Post post2 = postService.findById(2).get();
+
+        postService.delete(post1);
+
+        if(true) throw new RuntimeException("테스트용 예외 발생");
+
+        postService.delete(post2);
     }
 }
