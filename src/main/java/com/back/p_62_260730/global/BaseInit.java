d7357a6c35b662c3b10a9aa303b6a219249e6cb5
jeongsuji <sujijeong11@gmail.com>
2026-07-31 09:25:58 +0900

    show sql

--- src/main/java/com/back/p_62_260730/global/BaseInit.java
+++ src/main/java/com/back/p_62_260730/global/BaseInit.java
@@ -1,18 +1,23 @@
 package com.back.p_62_260730.global;
 
+import com.back.p_62_260730.domain.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 @Configuration //빈 등록을 위한 클래스
 public class BaseInit {
 
+    @Autowired
+    private PostRepository postRepository;
 
-    //람다 방식: 익명 클래스를 만들어 반환(MyApplicationRunner 없어도 됨)
+    //람다 방식
     @Bean
-    @Order(2)
     public ApplicationRunner init() {
-        return args -> System.out.println("초기 데이터를 로딩합니다");
+        return args -> {
+            System.out.println("post의 row 개수를 셉니다.");
+            postRepository.count();
+        };
     }
 }
