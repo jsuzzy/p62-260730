eb6b6cff71ece91114f6d90d4da1c5a98158087c
jeongsuji <sujijeong11@gmail.com>
2026-07-31 09:49:53 +0900

    findById

--- src/main/java/com/back/p_62_260730/global/BaseInit.java
+++ src/main/java/com/back/p_62_260730/global/BaseInit.java
@@ -7,6 +7,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
+
 @Configuration //빈 등록을 위한 클래스
 public class BaseInit {
 
@@ -17,16 +19,25 @@ public class BaseInit {
     @Bean
     public ApplicationRunner init() {
         return args -> {
-            System.out.println("post의 row 개수를 셉니다.");
-            postRepository.count();
+            System.out.println("초기화 작업을 수행합니다.");
+            if(postRepository.count() > 0){
+                return;
+            };
 
             //post 하나 저장
             Post post = new Post();
             post.setTitle("제목1");
             post.setBody("내용1");
             postRepository.save(post);
+            //insert into post ...
 
             //post 조회
+            Optional<Post> opPost =  postRepository.findById(1);
+
+            if(opPost.isPresent()){
+                System.out.println(opPost.get().getTitle());
+                System.out.println(opPost.get().getBody());
+            }
         };
     }
 }
