d70b9721488f0698e40bde4fff9a97125fabca1d
jeongsuji <sujijeong11@gmail.com>
2026-07-31 11:24:29 +0900

    service annotation

--- src/main/java/com/back/p_62_260730/domain/post/service/PostService.java
+++ src/main/java/com/back/p_62_260730/domain/post/service/PostService.java
@@ -3,11 +3,11 @@ package com.back.p_62_260730.domain.post.service;
 import com.back.p_62_260730.domain.post.entity.Post;
 import com.back.p_62_260730.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Component
+@Service
 @RequiredArgsConstructor
 public class PostService {
 
