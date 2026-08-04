a0d0cd9a6c52c7a2ecd495a8b7d1d2b7a1a6ddf9
jeongsuji <sujijeong11@gmail.com>
2026-08-04 10:31:42 +0900

    testDB

---
+++ src/test/java/com/back/p_62_260730/PostRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.back.p_62_260730;
+
+import com.back.p_62_260730.domain.post.entity.Post;
+import com.back.p_62_260730.domain.post.repository.PostRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class PostRepositoryTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @DisplayName("2번 글 조회")
+    void t1() {
+        Post post1 = postRepository.findById(2).get();
+
+        assertThat(post1.getId()).isEqualTo(2);
+        assertThat(post1.getTitle()).isEqualTo("제목2");
+        assertThat(post1.getBody()).isEqualTo("내용2");
+
+    }
+}
